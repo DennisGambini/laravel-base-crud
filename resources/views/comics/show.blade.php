d7df7ed61e32817f9287613b721ee870fcbf2d84
DennisGambini <dennis.gambini@gmail.com>
2022-06-15 17:33:08 +0200

    stile show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,7 +6,24 @@
 
 <div class="detail">
     <div class="container">
+
         <h1>{{$comic->title}}</h1>
+
+        <div class="image">
+            <img src="{{$comic->thumb}}" alt="immagine di {{$comic->title}}">
+        </div>
+
+        <h3 class="series">Serie {{$comic->series}}</h3>
+
+        <div class="description">{{$comic->description}}</div>
+
+        <div class="price">
+            <span style="font-size: 1.2em; font-weight: 600;">Price : </span>
+            {{$comic->price}} &dollar;
+        </div>
+
+        <div style="float: right" class="sale-date">Sale On {{$comic->sale_date}}</div>
+
     </div>
 </div>
 
