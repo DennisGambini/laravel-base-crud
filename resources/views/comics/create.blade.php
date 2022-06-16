87c2c13a02babc04d5ab1e770732144ce487a042
DennisGambini <dennis.gambini@gmail.com>
2022-06-16 17:52:27 +0200

    form di create ed edit migliorati

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,28 +12,44 @@
         <form action="{{route('comics.store')}}" method="POST">
             @csrf
 
-            <label for="title">Title</label>
-            <input type="text" id="title" name="title" placeholder="insert title">
+            <div class="input-box">
+                <label for="title">Title</label>
+                <input type="text" id="title" name="title" placeholder="insert title">
+            </div>
 
-            <label for="description">Description</label>
-            <input type="text" name="description" name="description" placeholder="insert description">
+            <div class="input-box">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" rows="10" placeholder="insert description"></textarea>
+            </div>
 
-            <label for="thumb">Image Url</label>
-            <input type="text" name="thumb" id="thumb" name="thumb" placeholder="insert image url">
+            <div class="input-box">
+                <label for="thumb">Image Url</label>
+                <input type="text" name="thumb" id="thumb" name="thumb" placeholder="insert image url">
+            </div>
 
-            <label for="price">Price with decimals</label>
-            <input type="number" name="price" id="price" name="price" placeholder="insert price (ex: 12.00)" min="1" max="1000">
+            <div class="input-box">
+                <label for="price">Price with decimals</label>
+                <input type="number" name="price" id="price" name="price" placeholder="insert price (ex: 12.00)" min="1" max="1000">
+            </div>
 
-            <label for="series">Series</label>
-            <input type="text" name="series" id="series" name="series" placeholder="insert series">
+            <div class="input-box">
+                <label for="series">Series</label>
+                <input type="text" name="series" id="series" name="series" placeholder="insert series">
+            </div>
 
-            <label for="sale_date">First Sale Date</label>
-            <input type="date" name="sale_date" id="sale_date" name="sale_date">
+            <div class="input-box">
+                <label for="sale_date">First Sale Date</label>
+                <input type="date" name="sale_date" id="sale_date" name="sale_date">
+            </div>
 
-            <label for="type">Series</label>
-            <input type="text" name="type" id="type" name="type" placeholder="insert series">
+            <div class="input-box">
+                <label for="type">Type</label>
+                <input type="text" name="type" id="type" name="type" placeholder="insert series">
+            </div>
 
-            <button type="submit">Save</button>
+            <div class="input-box">
+                <button type="submit" class="save">Save</button>
+            </div>
         
         </form>
 
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,30 +12,48 @@
         <form action="{{route('comics.update', $comic->id)}}" method="POST">
             @csrf
             @method('PUT')
-
-            <label for="title">Title</label>
-            <input type="text" id="title" name="title" placeholder="insert title" value="{{$comic->title}}">
-
-            <label for="description">Description</label>
-            <input type="text" name="description" name="description" placeholder="insert description" value="{{$comic->description}}">
-
-            <label for="thumb">Image Url</label>
-            <input type="text" name="thumb" id="thumb" name="thumb" placeholder="insert image url" value="{{$comic->thumb}}">
-
-            <label for="price">Price with decimals</label>
-            <input type="number" name="price" id="price" name="price" placeholder="insert price (ex: 12.00)" min="1" max="1000" value="{{$comic->price}}">
-
-            <label for="series">Series</label>
-            <input type="text" name="series" id="series" name="series" placeholder="insert series" value="{{$comic->series}}">
-
-            <label for="sale_date">First Sale Date</label>
-            <input type="date" name="sale_date" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
-
-            <label for="type">Type</label>
-            <input type="text" name="type" id="type" name="type" placeholder="insert series" value="{{$comic->type}}">
-
-            <button type="submit">Save</button>
         
+            <div class="input-box">
+                <label for="title">Title</label>
+                <input type="text" id="title" name="title" placeholder="insert title" value="{{$comic->title}}">
+            </div>
+
+            <div class="input-box">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" rows="10" placeholder="insert description">{{$comic->description}}</textarea>
+            </div>
+
+            <div class="input-box">
+                <label for="thumb">Image Url</label>
+                <input type="text" name="thumb" id="thumb" name="thumb" placeholder="insert image url" value="{{$comic->thumb}}">
+            </div>
+
+            <div class="input-box">
+                <label for="price">Price with decimals</label>
+                <input type="number" name="price" id="price" name="price" placeholder="insert price (ex: 12.00)" min="1" max="1000" value="{{$comic->price}}">
+            </div>
+
+            <div class="input-box">
+                <label for="series">Series</label>
+                <input type="text" name="series" id="series" name="series" placeholder="insert series" value="{{$comic->series}}">
+            </div>
+
+            <div class="input-box">
+                <label for="sale_date">First Sale Date</label>
+                <input type="date" name="sale_date" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+            </div>
+
+            <div class="input-box">
+                <label for="type">Type</label>
+                <input type="text" name="type" id="type" name="type" placeholder="insert series" value="{{$comic->type}}">
+            </div>
+
+            <div class="input-box">
+                <button type="submit" class="save">Save</button>
+            </div>
+
+
+
         </form>
 
     </div>
