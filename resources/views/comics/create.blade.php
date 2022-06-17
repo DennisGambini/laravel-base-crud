532ea2d1c07b316cb626e45079d9bab6e37c24d5
DennisGambini <dennis.gambini@gmail.com>
2022-06-17 15:09:28 +0200

    fine

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -14,7 +14,11 @@
 
             <div class="input-box">
                 <label for="title">Title</label>
-                <input type="text" id="title" name="title" placeholder="insert title" class="@error('title') input-error @enderror">
+                <input type="text" id="title" name="title" 
+                placeholder="insert title" 
+                class="@error('title') input-error @enderror" 
+                {{-- value="@error('title') {{old('title')}} @enderror" --}}
+                >
 
                 @error('title')
                     <div class="input-error">{{ $message }}</div>
@@ -32,7 +36,11 @@
 
             <div class="input-box">
                 <label for="thumb">Image Url</label>
-                <input type="text" name="thumb" id="thumb" name="thumb" placeholder="insert image url" class="@error('thumb') input-error @enderror">
+                <input type="text" name="thumb" id="thumb" name="thumb" 
+                placeholder="insert image url" 
+                class="@error('thumb') input-error @enderror" 
+                {{-- value="@error('thumb') {{old('thumb')}} @enderror" --}}
+                >
 
                 @error('thumb')
                     <div class="input-error">{{ $message }}</div>
@@ -41,7 +49,11 @@
 
             <div class="input-box">
                 <label for="price">Price with decimals</label>
-                <input type="number" name="price" id="price" name="price" placeholder="insert price (ex: 12.00)" min="1" max="1000" class="@error('price') input-error @enderror">
+                <input type="number" name="price" id="price" name="price" 
+                placeholder="insert price (ex: 12.00)" min="1" max="1000" 
+                class="@error('price') input-error @enderror"
+                {{-- value="@error('price') {{old('price')}} @enderror" --}}
+                >
 
                 @error('price')
                     <div class="input-error">{{ $message }}</div>
@@ -50,7 +62,11 @@
 
             <div class="input-box">
                 <label for="series">Series</label>
-                <input type="text" name="series" id="series" name="series" placeholder="insert series" class="@error('series') input-error @enderror">
+                <input type="text" name="series" id="series" name="series" 
+                placeholder="insert series" 
+                class="@error('series') input-error @enderror"
+                {{-- value="@error('series') {{old('series')}} @enderror" --}}
+                >
 
                 @error('series')
                     <div class="input-error">{{ $message }}</div>
@@ -59,7 +75,10 @@
 
             <div class="input-box">
                 <label for="sale_date">First Sale Date</label>
-                <input type="date" name="sale_date" id="sale_date" name="sale_date" class="@error('sale_date') input-error @enderror">
+                <input type="date" name="sale_date" id="sale_date" name="sale_date" 
+                class="@error('sale_date') input-error @enderror"
+                {{-- value="@error('sale_date') {{old('sale_date')}} @enderror" --}}
+                >
 
                 @error('sale_date')
                     <div class="input-error">{{ $message }}</div>
@@ -68,7 +87,11 @@
 
             <div class="input-box">
                 <label for="type">Type</label>
-                <input type="text" name="type" id="type" name="type" placeholder="insert series" class="@error('type') input-error @enderror">
+                <input type="text" name="type" id="type" name="type" 
+                placeholder="insert series" 
+                class="@error('type') input-error @enderror"
+                {{-- value="@error('type') {{old('type')}} @enderror" --}}
+                >
                 
                 @error('type')
                     <div class="input-error">{{ $message }}</div>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -22,37 +22,65 @@
         
             <div class="input-box">
                 <label for="title">Title</label>
-                <input type="text" id="title" name="title" placeholder="insert title" value="{{$comic->title}}">
+                <input type="text" id="title" name="title" placeholder="insert title" value="{{$comic->title}}" class="@error('title') input-error @enderror">
+                
+                @error('title')
+                    <div class="input-error">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="input-box">
                 <label for="description">Description</label>
-                <textarea name="description" id="description" rows="10" placeholder="insert description">{{$comic->description}}</textarea>
+                <textarea name="description" id="description" rows="10" placeholder="insert description" class="@error('description') input-error @enderror">{{$comic->description}}</textarea>
+                
+                @error('description')
+                    <div class="input-error">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="input-box">
                 <label for="thumb">Image Url</label>
-                <input type="text" name="thumb" id="thumb" name="thumb" placeholder="insert image url" value="{{$comic->thumb}}">
+                <input type="text" name="thumb" id="thumb" name="thumb" placeholder="insert image url" value="{{$comic->thumb}}" class="@error('thumb') input-error @enderror">
+                
+                @error('thumb')
+                    <div class="input-error">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="input-box">
                 <label for="price">Price with decimals</label>
-                <input type="number" name="price" id="price" name="price" placeholder="insert price (ex: 12.00)" min="1" max="1000" value="{{$comic->price}}">
+                <input type="number" name="price" id="price" name="price" placeholder="insert price (ex: 12.00)" min="1" max="1000" value="{{$comic->price}}" class="@error('price') input-error @enderror">
+                
+                @error('price')
+                    <div class="input-error">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="input-box">
                 <label for="series">Series</label>
-                <input type="text" name="series" id="series" name="series" placeholder="insert series" value="{{$comic->series}}">
+                <input type="text" name="series" id="series" name="series" placeholder="insert series" value="{{$comic->series}}" class="@error('series') input-error @enderror">
+                
+                @error('series')
+                    <div class="input-error">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="input-box">
                 <label for="sale_date">First Sale Date</label>
-                <input type="date" name="sale_date" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+                <input type="date" name="sale_date" id="sale_date" name="sale_date" value="{{$comic->sale_date}}" class="@error('sale_date') input-error @enderror">
+                
+                @error('sale_date')
+                    <div class="input-error">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="input-box">
                 <label for="type">Type</label>
-                <input type="text" name="type" id="type" name="type" placeholder="insert series" value="{{$comic->type}}">
+                <input type="text" name="type" id="type" name="type" placeholder="insert series" value="{{$comic->type}}" class="@error('type') input-error @enderror">
+                
+                @error('type')
+                    <div class="input-error">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="input-box">
